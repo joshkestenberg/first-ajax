8fc8b34b6ae1d8ded13935eb153bc99e85bde02d
joshkestenberg <joshkestenberg@gmail.com>
2017-02-16 17:05:08 -0500

    completed without last stretch assign. couldn't figure it out

--- ajax.js
+++ ajax.js
@@ -1,5 +1,89 @@
-$(document).ready(function () {
+$(function () {
 
-  /* Your code goes here */
+  $('#1').on('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/',
+      method: 'get',
+      dataType: 'text'
+    });
+  });
+
+  // $('#2').on('click', function(){
+  //   $.ajax({
+  //     url: 'http://first-ajax-api.herokuapp.com/ping',
+  //     method: 'get',
+  //     dataType: 'text'
+  //   }).done(function(responseData){
+  //     console.log(responseData);
+  //     $('#step3456').append(responseData);
+  //   }).fail(function(){
+  //     $('#step3456').append("It's all gone to poop..");
+  //   });
+  // });
+
+  $('#2').on('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/pong',
+      method: 'get',
+      dataType: 'text'
+    }).done(function(responseData){
+      console.log(responseData);
+      $('#step3456').append(responseData);
+    }).fail(function(jqHXR, textStatus, errorThrown){
+      $('#step3456').append(jqHXR.responseText);
+    }).always(function(){
+      console.log("No probz..");
+    });
+  });
+
+  $('#3').on('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'get',
+      data: {
+        amount: 3 //adds this number to count
+      },
+      dataType: 'text'
+    }).done(function(responseData){
+      console.log(responseData);
+      $('#step7').append(responseData);
+    }).fail(function(){
+      $('#step7').append("It's all gone to poop..");
+    }).always(function(){
+      console.log("No probz..");
+    });
+  });
+
+  $('#4').on('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'get',
+      data: {
+        timezone: 'Pacific/Honolulu'
+      },
+      dataType: 'text'
+    }).done(function(responseData){
+      console.log(responseData);
+      $('#step8').append(responseData);
+    }).fail(function(){
+      $('#step8').append("It's all gone to poop..");
+    }).always(function(){
+      console.log("No probz..");
+    });
+  });
+
+  $('#5').on('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/a_car',
+      method: 'get',
+      dataType: 'html'
+    }).done(function(responseData){
+      $('#ul1').append(responseData);
+    }).fail(function(){
+      $('#step9').append("It's all gone to poop..");
+    }).always(function(){
+      console.log("No probz..");
+    });
+  });
 
 });
